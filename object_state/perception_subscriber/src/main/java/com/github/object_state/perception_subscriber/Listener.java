298b857bcddb6fe4784fa7f70bd25a80fd518ffd
Michael <mspeer@uni-bremen.de>
2016-11-25 15:54:26 +0100

    :unicorn: perception_msgs hinzugefuegt, listener erweiter, Probleme beim builden, Montag mehr

--- object_state/perception_subscriber/src/main/java/com/github/object_state/perception_subscriber/Listener.java
+++ object_state/perception_subscriber/src/main/java/com/github/object_state/perception_subscriber/Listener.java
@@ -17,6 +17,7 @@
 package com.github.rosjava.object_state.perception_subscriber;
 
 import org.apache.commons.logging.Log;
+
 import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
@@ -24,25 +25,66 @@ import org.ros.node.ConnectedNode;
 import org.ros.node.NodeMain;
 import org.ros.node.topic.Subscriber;
 
+import geometry_msgs.Pose;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Quat4d;
+import javax.vecmath.Vector3d;
+
+import perception_msgs.ObjectDetection;
+
+import org.knowrob.prolog.PrologInterface;
+import org.ros.concurrent.CancellableLoop;
+
 /**
  * A simple {@link Subscriber} {@link NodeMain}.
  */
 public class Listener extends AbstractNodeMain {
+	
+	ConnectedNode node;
+	BlockingQueue<ObjectDetection> detections;
+	Thread updateKnowRobObjDetections;
+	
+	@Override
+	public GraphName getDefaultNodeName() {
+		return GraphName.of("rosjava/listener");
+	}
 
-  @Override
-  public GraphName getDefaultNodeName() {
-    return GraphName.of("rosjava/listener");
-  }
-
-  @Override
-  public void onStart(ConnectedNode connectedNode) {
-    final Log log = connectedNode.getLog();
-    Subscriber<std_msgs.String> subscriber = connectedNode.newSubscriber("chatter", std_msgs.String._TYPE);
-    subscriber.addMessageListener(new MessageListener<std_msgs.String>() {
-      @Override
-      public void onNewMessage(std_msgs.String message) {
-        log.info("I heard: \"" + message.getData() + "\"");
-      }
-    });
-  }
+	@Override
+	public void onStart(ConnectedNode connectedNode) {
+		// save reference to the ROS node
+		this.node = connectedNode;
+		
+		this.detections = new LinkedBlockingQueue<ObjectDetection>();
+		
+		
+		// wait for node to be ready
+		try {
+			while(node ==null) {
+				Thread.sleep(200);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		  
+	    final Log log = connectedNode.getLog();
+	    Subscriber<ObjectDetection> subscriber = connectedNode.newSubscriber("dummy_object_detections", ObjectDetection._TYPE);
+	    subscriber.addMessageListener(new MessageListener<ObjectDetection>() {
+	    	@Override
+	    	public void onNewMessage(perception_msgs.ObjectDetection message) {
+	    		try {
+	    			detections.put(message);
+	    			log.info("Logged \""+ message.getName() +"\"");
+	    			log.info("Detections now has "+ detections.getSize() +".");
+	    		} catch (InterruptedException e) {
+	    			e.printStackTrace();
+	    		}
+	    		log.info("I heard: \"" + message.getData() + "\"");
+	    	}
+	    });
+    
+	}
 }
