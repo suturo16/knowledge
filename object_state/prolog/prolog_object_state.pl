8fd12f266bed6aa0e2ef2125e00b36833909ed8b
Lukas S <luke22@fb3.uni-bremen.de>
2016-12-07 13:51:19 +0100

    Rework prolog function 'perceive_objects'

--- object_state/prolog/prolog_object_state.pl
+++ object_state/prolog/prolog_object_state.pl
@@ -21,18 +21,18 @@
 :- rdf_db:rdf_register_ns(knowrob,  'http://knowrob.org/kb/knowrob.owl#',  [keep(true)]).
  
 
-%% perceive_objects(+Name, +Pose, +Type, +Width, +Height, +Depth, -ObjInst) is det.
+%% perceive_objects(+Name, +PoseAsList, +Type, +Width, +Height, +Depth, -ObjInst) is det.
 %
 % Perceives the objects and creates the related objects in knowrob.
 % 
-% @param Name an identifier
-% @param Pose The pose of the object
-% @param Type The type of the object
+% @param Name describes the class of the object
+% @param PoseAsList The pose of the object stored in a list
+% @param Type The type of the object (see ObjectDetection.msg)
 % @param Width The width of the object
-% @param Height The height of red of the object
-% @param Depth The depth of green of the object
-% @param ObjInst The object instance to be returned
-perceive_objects(Name, Pose, Type, Width, Height, Depth, ObjInst) :- 
+% @param Height The height of the object
+% @param Depth The depth of the object
+% @param ObjInst The created object instance (optional:to be returned)
+perceive_objects(Name, PoseAsList, Type, Width, Height, Depth, ObjInst) :- 
     rdf_instance_from_class(Name, ObjInst),
     % needs to be modified
     create_perception_instance(Name, Perception),
