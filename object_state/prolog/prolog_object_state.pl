5b9b4f1a5155af3a2bf1cafb2e90fbf7693acb8c
Lukas S <luke22@fb3.uni-bremen.de>
2016-12-13 14:13:19 +0100

    Cleanup close_object_state function

--- object_state/prolog/prolog_object_state.pl
+++ object_state/prolog/prolog_object_state.pl
@@ -8,7 +8,7 @@
 :- module(prolog_object_state,
     [
       create_object_state/9,
-      close_object_state/9,
+      close_object_state/1,
       create_object_state_with_close/9,
       create_object_name/2,
       create_temporal_name/2,
@@ -26,7 +26,7 @@
       holds_suturo(r,?),
       dummy_perception(?).
 
-% sort of importing external libraries
+%importing external libraries
 :- use_module(library('semweb/rdf_db')).
 :- use_module(library('semweb/rdfs')).
 :- use_module(library('owl')).
@@ -70,28 +70,22 @@ create_object_state(Name, Pose, Type, Frame, Width, Height, Depth, [Begin], ObjI
     rdf_assert(Fluent, knowrob:'heightOfObject',literal(type(xsd:float, Height))),
     rdf_assert(Fluent, knowrob:'depthOfObject', literal(type(xsd:float, Depth))).
 
+
 create_object_state_with_close(Name, Pose, Type, Frame, Width, Height, Depth, [Begin], ObjInst) :-
-    ignore(close_object_state(Name, Pose, Type, Frame, Width, Height, Depth, [Begin,End], ObjInst)),
+    ignore(close_object_state(Name)),
     create_object_state(Name, Pose, Type, Frame, Width, Height, Depth, [Begin], ObjInst).
 
 
-%% close_object_state(+Name, +Pose, +Type, +Frame, +Width, +Height, +Depth, +[Begin,End], -ObjInst) is probably det.
-% Create the object representations in the knowledge base
-% Argument 'Type' specifies perceptions classification of the object
-% 
+%% close_object_state(+Name) is probably det.
+%
+% Closes the interval of a holding fluent 
 % @param Name describes the class of the object
-% @param PoseAsList The pose of the object stored in a list
-% @param Type The type of the object (see ObjectDetection.msg)
-% @param Width The width of the object
-% @param Height The height of the object
-% @param Depth The depth of the object
-% @param Interval A list containing the start time and end time of a temporal
-% @param ObjInst The created object instance (optional:to be returned)
-close_object_state(Name, Pose, Type, Frame, Width, Height, Depth, [Begin,End], ObjInst) :- 
+close_object_state(Name) :- 
     create_object_name(Name, FullName),
     owl_has(Obj,rdf:type,FullName),    
     fluent_assert_end(Obj,P).
     
+
 %% create_object_name(+Name,-FullName) is det.
 % Appends Name to 'http://knowrob.org/kb/knowrob.owl#'
 %
@@ -100,12 +94,14 @@ close_object_state(Name, Pose, Type, Frame, Width, Height, Depth, [Begin,End], O
 create_object_name(Name, FullName) :-
   atom_concat('http://knowrob.org/kb/knowrob.owl#', Name, FullName).
 
+
 %% create_temporal_name(+FullName, -FullTemporalName) is det.
 %
 % @param FullName full object name without temporal stamp  perception 
 % @returns FullTemporalName the concatenated string including the temporal stamp
 create_temporal_name(FullName, FullTemporalName) :-
   atom_concat(FullName,'@t_i', FullTemporalName).
+
 
 %% get_object_infos(+Name, -Frame, -Height, -Width, -Depth)
 %
@@ -126,13 +122,14 @@ get_object_infos(Name, Frame, Height, Width, Depth) :-
   owl_has(Fluent, knowrob:'depthOfObject', literal(type(xsd:float,Depth))).
     %atom_number(DepthStr, Depth).
 
+%% holds_suturo(+ObjInst, -Fluent)
 holds_suturo(ObjInst, Fluent) :-
   owl_has(ObjInst,knowrob:'temporalParts',Fluent),
   owl_has(Fluent,knowrob:'temporalExtend',I),
   current_time(Now),
   interval_during([Now,Now],I).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+%%
 % Dummy object_state
 dummy_perception(Name) :-
 	create_object_state(Name, Pose, 1.0, '/odom_combined', 20.0, 14.0, 9.0, Begin, ObjInst).
