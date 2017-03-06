3e71f093f4830faf097533b65d96a033c2d2f266
Sascha Jongebloed <sasjonge@uni-bremen.de>
2017-03-06 14:24:17 +0100

    :muscle: Improve of code structure.

--- object_state/scripts/subscriber.py
+++ object_state/scripts/subscriber.py
@@ -9,14 +9,17 @@ from suturo_perception_msgs.msg import ObjectDetection
 object_map = {}
 last_update_map = {}
 prolog = json_prolog.Prolog()
-#Timeout of prolog update in nanosekunden (50.000.000 sind also 0.05 sekunden, 66.666.666 ungefähr 15hz)
-timeout = 66666666
-# Distance of the position and orientation. 0.03 are 3%
-distance = 0.03
+# Timeout of prolog update in nanosekunden (50.000.000 sind also 0.05 sekunden, 66.666.666 ungefaehr 15hz)
+TIMEOUT = 65666666
+# Distance of the position and orientation. 
+POSE_DISTANCE = 0.01
+QUAT_DISTANCE = 0.01
 
 def callback(currObj):
 
-    if (not currObj.name in object_map) or (not isNear(currObj, object_map[currObj.name])) or (last_update_map[currObj.name].to_nsec() < rospy.Time.now().to_nsec() - timeout):
+    if ((not currObj.name in object_map) 
+        or (not isNear(currObj, object_map[currObj.name])) 
+        or (last_update_map[currObj.name].to_nsec() < rospy.Time.now().to_nsec() - TIMEOUT)):
         query = prolog.query("create_object_state_with_close('" 
                                 + currObj.name+"', [ ["
                                 + str(currObj.pose.pose.position.x) + "," 
@@ -38,7 +41,13 @@ def callback(currObj):
 
 
 def isNear(currObj, oldObj):
-    return (abs(currObj.pose.pose.position.x - oldObj.pose.pose.position.x) <= (oldObj.pose.pose.position.x*distance)) and (abs(currObj.pose.pose.position.y - oldObj.pose.pose.position.y) <= (oldObj.pose.pose.position.y*distance)) and (abs(currObj.pose.pose.position.z - oldObj.pose.pose.position.z) <= (oldObj.pose.pose.position.z*distance)) and (abs(currObj.pose.pose.orientation.x - oldObj.pose.pose.orientation.x) <= (oldObj.pose.pose.orientation.x*distance)) and (abs(currObj.pose.pose.orientation.y - oldObj.pose.pose.orientation.y) <= (oldObj.pose.pose.orientation.y*distance)) and (abs(currObj.pose.pose.orientation.z - oldObj.pose.pose.orientation.z) <= (oldObj.pose.pose.orientation.z*distance)) and (abs(currObj.pose.pose.orientation.w - oldObj.pose.pose.orientation.w) <= (oldObj.pose.pose.orientation.w*distance))
+    return ((abs(currObj.pose.pose.position.x - oldObj.pose.pose.position.x) <= (oldObj.pose.pose.position.x*POSE_DISTANCE)) 
+        and (abs(currObj.pose.pose.position.y - oldObj.pose.pose.position.y) <= (oldObj.pose.pose.position.y*POSE_DISTANCE)) 
+        and (abs(currObj.pose.pose.position.z - oldObj.pose.pose.position.z) <= (oldObj.pose.pose.position.z*POSE_DISTANCE)) 
+        and (abs(currObj.pose.pose.orientation.x - oldObj.pose.pose.orientation.x) <= (oldObj.pose.pose.orientation.x*QUAT_DISTANCE)) 
+        and (abs(currObj.pose.pose.orientation.y - oldObj.pose.pose.orientation.y) <= (oldObj.pose.pose.orientation.y*QUAT_DISTANCE)) 
+        and (abs(currObj.pose.pose.orientation.z - oldObj.pose.pose.orientation.z) <= (oldObj.pose.pose.orientation.z*QUAT_DISTANCE)) 
+        and (abs(currObj.pose.pose.orientation.w - oldObj.pose.pose.orientation.w) <= (oldObj.pose.pose.orientation.w*QUAT_DISTANCE)))
     
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
