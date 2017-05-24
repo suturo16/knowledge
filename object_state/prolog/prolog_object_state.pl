d96b702504d89483411e3064b4f9d9640f29b6dc
hasu <hawkin@informatik.uni-bremen.de>
2017-05-24 11:08:43 +0200

    :fix: fixing the quaternion bug

--- object_state/prolog/prolog_object_state.pl
+++ object_state/prolog/prolog_object_state.pl
@@ -376,19 +376,19 @@ dummy_perception2(Type) :-
 dummy_perception_with_close1(Type) :-
    % atom_concat(Type, '1', Name),
    get_time(TimeFloat),
-	 create_object_state_with_close(_, [[1.0,1.0,1.0],[0.0,0.0,0.0,0.0]], Type, '/odom_combined', 10.0, 10.0, 10.0, [TimeFloat], ObjInst).
+	 create_object_state_with_close(_, [[1.0,1.0,1.0],[0.0,0.0,0.0,1.0]], Type, '/odom_combined', 10.0, 10.0, 10.0, [TimeFloat], ObjInst).
 
 
 dummy_perception_with_close2(Type) :-
    % atom_concat(Type, '2', Name),
    get_time(TimeFloat),
-   create_object_state_with_close(_, [[0.0,0.0,0.0],[0.0,0.0,0.0,0.0]], Type, '/odom_combined', 1.0, 1.0, 1.0, [TimeFloat], ObjInst).
+   create_object_state_with_close(_, [[0.0,0.0,0.0],[0.0,0.0,0.0,1.0]], Type, '/odom_combined', 1.0, 1.0, 1.0, [TimeFloat], ObjInst).
 
 
 dummy_perception_with_close3(Type) :-
    % atom_concat(Type, '2', Name),
    get_time(TimeFloat),
-   create_object_state_with_close(_, [[3.0,2.0,1.0],[0.0,0.0,0.0,0.0]], Type, '/odom_combined', 2.0, 1.0, 2.0, [TimeFloat], ObjInst).
+   create_object_state_with_close(_, [[3.0,2.0,1.0],[0.0,0.0,0.0,1.0]], Type, '/odom_combined', 2.0, 1.0, 2.0, [TimeFloat], ObjInst).
 
 
 dummy_close(Name) :-
