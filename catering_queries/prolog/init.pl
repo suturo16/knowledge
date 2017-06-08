365639092939cbd9c053180f9bb56b653e750262
Sascha Jongebloed <sasjonge@uni-bremen.de>
2017-06-08 18:11:05 +0200

    :unicorn: Added reasoning over possible recipes

--- catering_queries/prolog/init.pl
+++ catering_queries/prolog/init.pl
@@ -15,5 +15,9 @@
 % parse OWL files, register name spaces
 
 :- owl_parse('package://knowrob_common/owl/knowrob.owl').
+:- owl_parse('package://knowrob_common/owl/knowrob_common.owl').
+
 :- owl_parse('package://object_state/owl/suturo_objects.owl').
+:- owl_parse('package://catering_queries/owl/suturo_recipes.owl').
+
 :- use_module(library('knowrob_owl')).
