2f0034e9bf126c82b5e145f0145a2b4578703146
Sascha Jongebloed <sasjonge@uni-bremen.de>
2017-07-29 00:20:40 +0200

    :bug: Fixed small fix for tf call in connect frames

--- object_state/scripts/call_tf.py
+++ object_state/scripts/call_tf.py
@@ -7,6 +7,6 @@ def get_transform(parent_frame,child_frame):
     rospy.init_node('connect_frames_node')
     listener = tf.TransformListener()
     rospy.sleep(0.25)
-    listener.waitForTransform(parent_frame, child_frame, rospy.Time.now(), rospy.Duration(4.5))
+    listener.waitForTransform(parent_frame, child_frame, rospy.Time(0), rospy.Duration(4.5))
     (trans,rot) = listener.lookupTransform(parent_frame, child_frame, rospy.Time(0))
     return [list(trans), list(rot)]
